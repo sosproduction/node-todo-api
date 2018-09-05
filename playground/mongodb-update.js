957720eb5b75062222e1dd2fe150bb78f4a00d96
Rachel Geisman <rachel.geisman@protonmail.com>
2018-09-05 10:02:03 -0500

    add update methods

---
+++ playground/mongodb-update.js
@@ -0,0 +1,43 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  /*
+  db.collection('Todos').findOneAndUpdate({
+    _id: new ObjectID('5b8fe580975390e77a690e93')
+  }, {
+    $set: {
+      completed: true
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+      console.log(result);
+  });
+  */
+
+   db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5b8ee43a40b6bc6dad14e59d')
+  }, {
+    $set: {
+      name: 'Hannah Geisman'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+      console.log(result);
+  });
+  
+  // db.close();
+});
+
+
+
